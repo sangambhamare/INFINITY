68425fbf2922f857adce8f24f5b9214362489d38
Sangam Bhamare <84763687+sangambhamare@users.noreply.github.com>
2025-02-04 20:22:57 +0000

    Create scipt.js

---
+++ scipt.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("Interactive Login Page Loaded");
+  
+  // You can add more JavaScript to enhance interactivity
+  // For example, you might validate form inputs or animate elements
+});
